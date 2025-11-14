0c4cd20f808f11914af7469c97ae35c59a9187c1
Gustavo Chaves <146135610+Gunoch@users.noreply.github.com>
2025-11-14 17:18:40 -0300

    first commit

---
+++ galeria/src/data/images.js
@@ -0,0 +1,32 @@
+export const IMAGES =  [
+    {
+        id: 1,
+        title: "Marte",
+        description: "O planeta vermelho vheio de Aliens.",
+        src: "/imgs/marte.jpg"
+    },
+    {
+        id: 2,
+        title: "Júpiter",
+        description: "O gidante gasoso do Sistema Solar.",
+        src: "/imgs/jupiter.jpg"
+    },
+    {
+        id: 3,
+        title: "Saturno",
+        description: "Conhecido pelos seus anéis impressionantes.",
+        src: "/imgs/saturno.jpg"
+    },
+    {
+        id: 4,
+        title: "Nebulosa",
+        description: "Uma nuvem colorida de gás e poeira espacial.",
+        src: "/imgs/nebulosa.jpg"
+    },
+    {
+        id: 5,
+        title: "Galáxia",
+        description: "Um aglomerado de bilhões de estrelas.",
+        src: "/imgs/galaxia.jpg"
+    },
+];
